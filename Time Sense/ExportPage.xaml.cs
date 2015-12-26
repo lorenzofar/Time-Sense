2ea7f9208166773a012ddbe13df3bc23feb69b86
Lorenzo Farinelli <lori.gami@live.it>
2015-12-26 12:55:08 +0100

    Added file save picker to export page.

--- Time Sense/ExportPage.xaml.cs
+++ Time Sense/ExportPage.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Syncfusion.XlsIO;
 using Windows.Storage;
+using Windows.Storage.Pickers;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -30,42 +31,48 @@ namespace Time_Sense
         }
 
         protected override async void OnNavigatedTo(NavigationEventArgs e)
-        {
-            ExcelEngine excel_engine = new ExcelEngine();
-            IApplication application = excel_engine.Excel;
-            application.DefaultVersion = ExcelVersion.Excel2013;
-            IWorkbook workbook = application.Workbooks.Create(1);
-            IWorksheet worksheet = workbook.Worksheets[0];
-            //Adding text data
-            worksheet.Range["A1"].Text = "Month";
-            worksheet.Range["B1"].Text = "Sales";
-            worksheet.Range["A6"].Text = "Total";
-            //Adding DateTime data
-            worksheet.Range["A2"].DateTime = new DateTime(2015, 1, 10);
-            worksheet.Range["A3"].DateTime = new DateTime(2015, 2, 10);
-            worksheet.Range["A4"].DateTime = new DateTime(2015, 3, 10);
-            //Applying number format for date value cells A2 to A4
-            worksheet.Range["A2:A4"].NumberFormat = "mmmm, yyyy";
-            //Auto-size the first column to fit the content
-            worksheet.AutofitColumn(1);
-            //Adding numeric data
-            worksheet.Range["B2"].Number = 68878;
-            worksheet.Range["B3"].Number = 71550;
-            worksheet.Range["B4"].Number = 72808;
-            //Adding formula
-            worksheet.Range["B6"].Formula = "SUM(B2:B4)";
-            StorageFile doc = await KnownFolders.VideosLibrary.CreateFileAsync("timesense_export_test.xlsx", CreationCollisionOption.ReplaceExisting);
-            await workbook.SaveAsAsync(doc);
-            // Closing the workbook.
-            workbook.Close();
-            // Dispose the Excel engine
-            excel_engine.Dispose();
+        {            
 
         }
 
-        private void save_btn_Click(object sender, RoutedEventArgs e)
+        private async void save_btn_Click(object sender, RoutedEventArgs e)
         {
-
+            FileSavePicker export_picker = new FileSavePicker();
+            export_picker.DefaultFileExtension = ".xlsx";
+            export_picker.FileTypeChoices.Add("Excel file", new List<string>() { ".xlsx" });
+            StorageFile export_file = await export_picker.PickSaveFileAsync();
+            if (export_file != null)
+            {
+                ExcelEngine excel_engine = new ExcelEngine();
+                IApplication application = excel_engine.Excel;
+                application.DefaultVersion = ExcelVersion.Excel2013;
+                IWorkbook workbook = application.Workbooks.Create(1);
+                IWorksheet worksheet = workbook.Worksheets[0];
+                //Adding text data
+                worksheet.Range["A1"].Text = "Month";
+                worksheet.Range["B1"].Text = "Sales";
+                worksheet.Range["A6"].Text = "Total";
+                //Adding DateTime data
+                worksheet.Range["A2"].DateTime = new DateTime(2015, 1, 10);
+                worksheet.Range["A3"].DateTime = new DateTime(2015, 2, 10);
+                worksheet.Range["A4"].DateTime = new DateTime(2015, 3, 10);
+                //Applying number format for date value cells A2 to A4
+                worksheet.Range["A2:A4"].NumberFormat = "mmmm, yyyy";
+                //Auto-size the first column to fit the content
+                worksheet.AutofitColumn(1);
+                //Adding numeric data
+                worksheet.Range["B2"].Number = 68878;
+                worksheet.Range["B3"].Number = 71550;
+                worksheet.Range["B4"].Number = 72808;
+                //Adding formula
+                worksheet.Range["B6"].Formula = "SUM(B2:B4)";
+                await workbook.SaveAsAsync(export_file);
+                // Closing the workbook.
+                workbook.Close();
+                // Dispose the Excel engine
+                excel_engine.Dispose();
+            }
         }
+        
     }
 }
